20c070adc0c87ce80f0520da54683c7a27cd1f6d
sungho <tjdgh1231@tpirates.com>
2023-09-01 22:43:38 +0900

    jar 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("java")
 }
 
 group = "org.example"
@@ -10,12 +11,21 @@ repositories {
     mavenCentral()
 }
 
-val jar: Jar by tasks
-jar.enabled = true
+tasks.jar {
+    manifest.attributes["Main-Class"] = "MainKt"
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
 
 dependencies {
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("com.github.pengrad:java-telegram-bot-api:6.8.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     val ktorVersion = "2.1.3"
     // CIO request
