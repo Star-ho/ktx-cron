5473db2108dc35fb5474c958880e77329d8f1290
sungho <tjdgh1231@tpirates.com>
2023-09-25 14:37:26 +0900

    수영 추가

--- src/main/kotlin/SinlimSwimHttpRequest.kt
+++ src/main/kotlin/SinlimSwimHttpRequest.kt
@@ -14,6 +14,7 @@ class SinlimSwimHttpRequest {
             .setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
             .setHeader("Content-Type","application/x-www-form-urlencoded")
             .setHeader("Accept-Encoding","gzip, deflate, br")
+            .setHeader("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
             .build()
         val bodyString = client.send(request, HttpResponse.BodyHandlers.ofString()).body()
         println(bodyString)
